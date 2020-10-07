146de8699865e91a031ca3a219a7c88265772863
unbeliebter <svenpra2013@gmail.com>
2020-10-07 22:56:52 +0200

    cant do the recursive things

--- src/homework/ForestRekursive.java
+++ src/homework/ForestRekursive.java
@@ -1,4 +1,25 @@
 package homework;
 
 public class ForestRekursive {
+    public static void main(String[] args) {
+
+        double forestVolume = 60000;
+        double result = ForestRekursiveMethod(forestVolume);
+        System.out.println(result);
+
+    }
+
+    public static double ForestRekursiveMethod (double forestVolume) {
+
+       double result = forestVolume;
+
+        while (true) {
+            result = result * 1.05 - 3500;
+            System.out.println(result);
+
+            if (result < 1) {
+                return 1;
+            }
+        }
+    }
 }
