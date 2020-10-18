ab7d3d075994be7cdd0617631f310c464bee8c4c
unbeliebter <svenpra2013@gmail.com>
2020-10-19 01:07:02 +0200

    Updated InfectionCalculator

--- src/Calculator/InfectionsCalculator.java
+++ src/Calculator/InfectionsCalculator.java
@@ -6,5 +6,6 @@ public class InfectionsCalculator {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String reproductionNumber = scanner.nextLine();
     }
 }
