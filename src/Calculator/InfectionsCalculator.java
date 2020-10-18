fc5faa8b3c148307ad8569615163cc4f579b393f
unbeliebter <svenpra2013@gmail.com>
2020-10-19 01:04:36 +0200

    add Scanner in InfectionCalculator

--- src/Calculator/InfectionsCalculator.java
+++ src/Calculator/InfectionsCalculator.java
@@ -1,7 +1,10 @@
 package Calculator;
 
+import java.util.Scanner;
+
 public class InfectionsCalculator {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
     }
 }
