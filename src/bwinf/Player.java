12fa4036d47089c2e411e5162a9969950f43b97e
unbeliebter <svenpra2013@gmail.com>
2020-11-20 21:58:49 +0100

    implemented liga system

---
+++ src/bwinf/Player.java
@@ -0,0 +1,9 @@
+package bwinf;
+
+public class Player {
+    public String number;
+
+    public Player(String number) {
+        this.number = number;
+    }
+}
--- src/bwinf/TobisGame.java
+++ src/bwinf/TobisGame.java
@@ -3,17 +3,21 @@ package bwinf;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class TobisGame {
     public static void main(String[] args) {
 
+        ArrayList<String> players = new ArrayList<>();
+
         System.out.println("Gebe Pfad ein:");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
 
-        HashMap<String, String> players = new HashMap<String, String>();
+        System.out.println("Wiederholungen:");
+        int repetitions = scanner.nextInt();
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path));
@@ -21,13 +25,66 @@ public class TobisGame {
             int amountofPlayers = Integer.parseInt(reader.readLine());
 
             for (int i = 0; amountofPlayers != i; i++) {
-
-                players.put(Integer.toString(i), reader.readLine());
+                players.add(reader.readLine());
             }
             
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(players);
+
+        for (int i = 0; i < repetitions; i++) {
+            liga(players);
+
+        }
+
+    }
+
+    public static void liga(ArrayList players) {
+
+        ArrayList<String> wins = new ArrayList<>();
+
+        for (int i = 0; i < players.size(); i++) {
+            wins.add(i, "0");
+
+        }
+
+        Object playerOne = "";
+        Object playerTwo = "";
+        int strengthOfPlayerOne;
+        int strengthOfPlayerTwo;
+        int playerPositionOne;
+        int playerPositionTwo;
+        int winNumber;
+
+        for (int i = 0; i < players.size(); i++) {
+            playerOne = players.get(i);
+            strengthOfPlayerOne = Integer.valueOf((String) playerOne);
+            playerPositionOne = i;
+
+            for (int j = 0; j < players.size(); j++) {
+                playerTwo = players.get(j);
+                strengthOfPlayerTwo = Integer.valueOf((String) playerTwo);
+                playerPositionTwo = j;
+
+                double randomNumber = Math.random()*(strengthOfPlayerOne + strengthOfPlayerTwo);
+
+                if (randomNumber < strengthOfPlayerTwo) {
+                    winNumber = Integer.parseInt(wins.get(playerPositionOne)) + 1;
+                    wins.set(playerPositionOne, Integer.toString(winNumber));
+                } else if (randomNumber > strengthOfPlayerOne) {
+                    winNumber = Integer.parseInt(wins.get(playerPositionTwo)) + 1;
+                    wins.set(playerPositionTwo, Integer.toString(winNumber));
+                }
+
+            }
+        }
+        String winnerOfLiga = Collections.max(wins);
+        int winner = wins.indexOf(winnerOfLiga) + 1;
+
+        System.out.println("WINNER:" + winner);
+
+        System.out.println(wins + " | " + players);
     }
 }
+
+//H:\java.projects\src\bwinf\spielstaerken.txt
