b8f4c862b360221831e42e4e8c9392efd0fde6cd
unbeliebter <svenpra2013@gmail.com>
2020-11-21 13:23:03 +0100

    implemented first passwordGenMethod

--- src/bwinf/passwords/password.java
+++ src/bwinf/passwords/password.java
@@ -1,4 +1,27 @@
 package bwinf.passwords;
 
+import java.util.Scanner;
+
 public class password {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe für die erste Methode deines Passwortes Wörter an");
+        String words = scanner.nextLine();
+
+        String output = passwordGenFirst(words);
+        System.out.println(output);
+    }
+
+    public static String passwordGenFirst(String words) {
+        words = words.replace("e", "3");
+        words = words.replace("a", "@");
+        words = words.replace("o", "0");
+        words = words.replace("l", "1");
+        words = words.replace(" ", "#");
+        words = words.replace("i", "!");
+
+        return words;
+
+    }
 }
