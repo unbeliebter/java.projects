0c31dac077caf1b8d42a9f1ee87c469fa15226ea
Sven <42941230+unbeliebter@users.noreply.github.com>
2020-12-01 23:16:28 +0100

    created a frame

--- src/calculator/CalculatorJFrame.java
+++ src/calculator/CalculatorJFrame.java
@@ -1,4 +1,19 @@
 package calculator;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class CalculatorJFrame {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculator");
+        frame.setSize(400, 250);
+        frame.setTitle("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(59,59,59));
+
+    }
 }
