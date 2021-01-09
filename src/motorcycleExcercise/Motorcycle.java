b9c27d89875ccf54fc19bec44feefcbba6052e79
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-01-09 02:37:54 +0100

    implemented the Motorcycle

--- src/motorcycleExcercise/Motorcycle.java
+++ src/motorcycleExcercise/Motorcycle.java
@@ -1,4 +1,16 @@
 package motorcycleExcercise;
 
 public class Motorcycle {
+
+    public int kilometers;
+    public double tank;
+    public String name;
+    public double consumption;
+
+    public Motorcycle(int kilometers, double tank, String name, double consumption) {
+        this.kilometers = kilometers;
+        this.tank = tank;
+        this.name = name;
+        this.consumption = consumption;
+    }
 }
