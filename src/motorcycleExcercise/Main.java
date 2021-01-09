b554b4660e17c955a1a449b8a69a0ba9f75c43f7
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-01-09 02:38:10 +0100

    implemeted all methods given

--- src/motorcycleExcercise/Main.java
+++ src/motorcycleExcercise/Main.java
@@ -1,4 +1,29 @@
 package motorcycleExcercise;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Motorcycle motorcycle = new Motorcycle(2352, 8.4, "Flitzer", 2.5);
+
+        double distance = 222;
+        double price = 1.20;
+
+        double resultCosts = costsPerDistance(motorcycle, distance, price);
+        double resultRange = range(motorcycle);
+        double resultRefill = refill(motorcycle, price);
+
+        System.out.println(resultCosts + " " + resultRange + " " + resultRefill);
+    }
+
+    public static double costsPerDistance(Motorcycle motorcycle, double distance, double price) {
+        return ((distance / 100) * motorcycle.consumption) * price;
+    }
+
+    public static double range(Motorcycle motorcycle) {
+        return (motorcycle.tank / motorcycle.consumption) * 100;
+    }
+
+    public static double refill(Motorcycle motorcycle, double price) {
+        return motorcycle.tank * price;
+    }
 }
